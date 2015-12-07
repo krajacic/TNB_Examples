5f0be3d5e3f60e900ed018670010e80c9e2ae691
Jurica Krajačić <krajacic_jurica@yahoo.com>
2015-12-07 16:13:28 +0100

    Connecting Java and XML layouts

--- app/src/main/java/cawabanga/com/tnb_examples/MainActivity.java
+++ app/src/main/java/cawabanga/com/tnb_examples/MainActivity.java
@@ -1,13 +1,28 @@
 package cawabanga.com.tnb_examples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter;
+    Button add, sub;
+    TextView display;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        counter = 0;
+        add = (Button) findViewById(R.id.add_button);
+        sub = (Button) findViewById(R.id.sub_button);
+        display = (TextView) findViewById(R.id.count);
+
+
+
     }
 }
