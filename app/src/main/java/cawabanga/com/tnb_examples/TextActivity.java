a21c4c2db7ac9f7db1caf59eb17aa0e28d32dcb7
Jurica Krajačić <krajacic_jurica@yahoo.com>
2015-12-10 15:00:54 +0100

    Examples of if/else/if else statements with using Random

--- app/src/main/java/cawabanga/com/tnb_examples/TextActivity.java
+++ app/src/main/java/cawabanga/com/tnb_examples/TextActivity.java
@@ -1,5 +1,6 @@
 package cawabanga.com.tnb_examples;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.InputType;
@@ -9,6 +10,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ToggleButton;
+
+import java.util.Random;
 
 /**
  * Created by croatan on 9.12.2015..
@@ -41,12 +44,24 @@ public class TextActivity extends AppCompatActivity{
             @Override
             public void onClick(View v) {
                 String check = input.getText().toString();
+                display.setText(check);
                 if (check.contentEquals("left")){
                     display.setGravity(Gravity.LEFT);
                 }else if(check.contentEquals("center")){
                     display.setGravity(Gravity.CENTER);
                 }else if(check.contentEquals("right")){
                     display.setGravity(Gravity.RIGHT);
+                }else if(check.contentEquals("blue")){
+                    display.setTextColor(Color.BLUE);
+                }else if(check.contains("WTF")){
+                    Random crazy = new Random();
+                    display.setText("WTF!!!!");
+                    display.setTextSize(crazy.nextInt(75));
+                    display.setTextColor(Color.rgb(crazy.nextInt(265),crazy.nextInt(265),crazy.nextInt(265)));
+                }else{
+                    display.setText("invalid");
+                    display.setGravity(Gravity.CENTER);
+
                 }
             }
         });
