f6347687d723852a3e20978295142211a49a4901
Jurica Krajačić <krajacic_jurica@yahoo.com>
2015-12-11 15:51:07 +0100

    Setting up An Email Activity

--- app/src/main/java/cawabanga/com/tnb_examples/Menu.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Menu.java
@@ -12,7 +12,7 @@ import android.widget.ListView;
  */
 public class Menu extends ListActivity {
 
-    String classes[] = { "BgMusicLayoutActivity", "TextActivity", "example2", "example3", "example4", "example5", "example6" };
+    String classes[] = { "BgMusicLayoutActivity", "TextActivity", "Email", "example3", "example4", "example5", "example6" };
 
 
 
