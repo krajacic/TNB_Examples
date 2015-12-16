41caece1f89b5054608bb9212f48e6e5725808fb
Jurica Krajačić <krajacic_jurica@yahoo.com>
2015-12-16 16:43:09 +0100

    Animating a Bitmap and adding Rect

--- app/src/main/java/cawabanga/com/tnb_examples/MyBringBack.java
+++ app/src/main/java/cawabanga/com/tnb_examples/MyBringBack.java
@@ -5,6 +5,8 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 /**
@@ -13,18 +15,32 @@ import android.view.View;
 public class MyBringBack extends View {
 
     Bitmap gBall;
+    float changingY;
 
 
     public MyBringBack(Context context) {
         super(context);
 
         gBall = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+        changingY = 0;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawColor(Color.LTGRAY);
-        canvas.drawBitmap(gBall, (canvas.getWidth()/2), 0, null);
+        canvas.drawBitmap(gBall, (canvas.getWidth() / 2), changingY, null);
+        if (changingY < canvas.getHeight()) {
+            changingY += 10;
+        } else {
+            changingY = 0;
+        }
+        Rect middleRect = new Rect();
+        middleRect.set(0, 400, canvas.getWidth(), 500);
+        Paint ourBlue = new Paint();
+        ourBlue.setColor(Color.BLUE);
+        canvas.drawRect(middleRect, ourBlue);
+
+        invalidate(); //for loop
     }
 }
