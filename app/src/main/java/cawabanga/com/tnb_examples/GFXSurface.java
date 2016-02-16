f895b54e4ed6c31e18eb1eedb0aa4f44e446b9bc
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-02-16 17:05:11 +0100

    Defining a Class within a Class tut71 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/GFXSurface.java
+++ app/src/main/java/cawabanga/com/tnb_examples/GFXSurface.java
@@ -1,8 +1,14 @@
 package cawabanga.com.tnb_examples;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
 /**
@@ -41,5 +47,54 @@ public class GFXSurface extends AppCompatActivity implements View.OnTouchListene
         y = event.getY();
         return false;
     }
+
+    public class MyBringBackSurface extends SurfaceView implements Runnable{
+        SurfaceHolder ourHolder;
+        Thread ourThread = null;
+        boolean isRunning = false;
+
+        public MyBringBackSurface(Context context){
+            super(context);
+            ourHolder = getHolder();
+        }
+
+        public void pause(){
+            isRunning = false;
+            while (true){
+                try {
+                    ourThread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+
+            ourThread = null;
+        }
+        public void resume(){
+            isRunning = true;
+            ourThread = new Thread(this);
+            ourThread.start();
+        }
+
+        @Override
+        public void run() {
+            while (isRunning){
+                if (!ourHolder.getSurface().isValid())
+                    continue;
+
+                Canvas canvas = ourHolder.lockCanvas();
+                canvas.drawRGB(2, 2, 150);
+                if (x!=0 && y!=0){
+                    Bitmap test = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+                    canvas.drawBitmap(test, x, y, null);
+                }
+                ourHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+    }
+
+
+
 }
 
