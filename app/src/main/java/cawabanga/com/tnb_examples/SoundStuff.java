0ad56693e1b394def73e5ea5638e4737a368efa0
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-02-23 20:18:22 +0100

    Using the OnLongClick method - tut80 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/SoundStuff.java
+++ app/src/main/java/cawabanga/com/tnb_examples/SoundStuff.java
@@ -1,6 +1,7 @@
 package cawabanga.com.tnb_examples;
 
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +10,10 @@ import android.view.View;
 /**
  * Created by croatan on 23.2.2016. TNB_Examples.
  */
-public class SoundStuff extends AppCompatActivity implements View.OnClickListener {
+public class SoundStuff extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
 
     SoundPool sp;
+    MediaPlayer mp;
     int explosion = 0;
 
     @Override
@@ -19,9 +21,11 @@ public class SoundStuff extends AppCompatActivity implements View.OnClickListene
         super.onCreate(savedInstanceState);
         View v = new View(this);
         v.setOnClickListener(this);
+        v.setOnLongClickListener(this);
         setContentView(v);
-        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
+        sp = new SoundPool(5, AudioManager.STREAM_MUSIC, 0); //SoundPool is using for short plays like effects
         explosion = sp.load(this, R.raw.booom, 1);
+        mp = MediaPlayer.create(this, R.raw.radio);
 
     }
 
@@ -29,4 +33,10 @@ public class SoundStuff extends AppCompatActivity implements View.OnClickListene
         if (explosion != 0)
         sp.play(explosion, 1, 1, 0, 0,1);
     }
+
+    public boolean onLongClick(View arg0){
+        mp.start();
+        return false;
+    }
+
 }
