c1915f10155926cb1bebdacc65d352f278503229
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-01 13:44:11 +0100

    Creating Tabs in Java - tut86 TNB Also we setup Button for automaticly add new Tab with name "NEW" and return TextView...but we can return layout or something else...

--- app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
@@ -2,17 +2,31 @@ package cawabanga.com.tnb_examples;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 /**
  * Created by croatan on 1.3.2016. TNB_Examples.
  */
-public class Tabs extends AppCompatActivity {
+public class Tabs extends AppCompatActivity implements View.OnClickListener{
+
+    TabHost th;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
-        TabHost th = (TabHost) findViewById(R.id.tabHost);
+        th = (TabHost) findViewById(R.id.tabHost);
+
+        Button newTab = (Button) findViewById(R.id.bAddTab);
+        Button bStop = (Button) findViewById(R.id.bStopWatch);
+        Button bStart = (Button) findViewById(R.id.bStartWatch);
+        newTab.setOnClickListener(this);
+
+
+
         th.setup();
         TabHost.TabSpec specs = th.newTabSpec("tag1");
         specs.setContent(R.id.tab1);
@@ -28,5 +42,31 @@ public class Tabs extends AppCompatActivity {
         specs.setContent(R.id.tab3);
         specs.setIndicator("Add a Tab");
         th.addTab(specs);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bAddTab:
+                TabHost.TabSpec ourSpec = th.newTabSpec("tag1");
+                ourSpec.setContent(new TabHost.TabContentFactory(){
+                    @Override
+                    public View createTabContent(String tag) {
+                        TextView text = new TextView(Tabs.this);
+                        text.setText("You've created new tab!");
+                        return text;
+                    }
+                });
+                ourSpec.setIndicator("New");
+                th.addTab(ourSpec);
+            break;
+
+            case R.id.bStartWatch:
+
+            break;
+            case R.id.bStopWatch:
+
+            break;
+        }
     }
 }
