64a13cb01eb584030616ead41b8c0dfab01a2acb
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-01 12:06:48 +0100

    Methods of the SlidingDrawer - tut83 TNB We setup that: -Button1 Open our slider -Button2 do nothing :D -Button3 toogle our slider -Button4 close our slider If CheckBox is checked we can not close or do any action with slider Also we setup when slider open MediaPlayer mp start Boom sound.

--- app/src/main/java/cawabanga/com/tnb_examples/Slider.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Slider.java
@@ -1,15 +1,70 @@
 package cawabanga.com.tnb_examples;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SlidingDrawer;
 
 /**
  * Created by croatan on 1.3.2016. TNB_Examples.
  */
-public class Slider extends AppCompatActivity {
+public class Slider extends AppCompatActivity implements View.OnClickListener, CompoundButton.OnCheckedChangeListener, SlidingDrawer.OnDrawerOpenListener {
+    SlidingDrawer sd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sliding);
+        Button handle1 = (Button) findViewById(R.id.handle1);
+        Button handle2 = (Button) findViewById(R.id.handle2);
+        Button handle3 = (Button) findViewById(R.id.handle3);
+        Button handle4 = (Button) findViewById(R.id.handle4);
+        CheckBox checkbox = (CheckBox) findViewById(R.id.cbSlidable);
+        checkbox.setOnCheckedChangeListener(this);
+        sd = (SlidingDrawer) findViewById(R.id.slidingDrawer);
+        sd.setOnDrawerOpenListener(this);
+        handle1.setOnClickListener(this);
+        handle2.setOnClickListener(this);
+        handle3.setOnClickListener(this);
+        handle4.setOnClickListener(this);
+    }
+
+    public void onClick(View arg0) {
+        switch (arg0.getId()) {
+            case R.id.handle1:
+                sd.open();
+                break;
+            case R.id.handle2:
+
+                break;
+            case R.id.handle3:
+                sd.toggle();
+                break;
+            case R.id.handle4:
+                sd.close();
+                break;
+        }
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+        if (arg0.isChecked()) {
+            sd.lock();
+        } else {
+            sd.unlock();
+        }
+    }
+
+    @Override
+    public void onDrawerOpened() {
+        MediaPlayer mp = MediaPlayer.create(this, R.raw.booom);
+        mp.start();
     }
 }
+
+
+
