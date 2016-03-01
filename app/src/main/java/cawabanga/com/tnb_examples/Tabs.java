97577b54b7d445e78f845d73c4b6c437d1a9bd73
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-01 14:01:28 +0100

    Getting the Time from the System - tut87 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
@@ -13,19 +13,22 @@ import android.widget.TextView;
 public class Tabs extends AppCompatActivity implements View.OnClickListener{
 
     TabHost th;
+    TextView showResults;
+    long start, stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
         th = (TabHost) findViewById(R.id.tabHost);
+        showResults = (TextView) findViewById(R.id.tvShowResults);
 
         Button newTab = (Button) findViewById(R.id.bAddTab);
         Button bStop = (Button) findViewById(R.id.bStopWatch);
         Button bStart = (Button) findViewById(R.id.bStartWatch);
+        bStop.setOnClickListener(this);
+        bStart.setOnClickListener(this);
         newTab.setOnClickListener(this);
-
-
 
         th.setup();
         TabHost.TabSpec specs = th.newTabSpec("tag1");
@@ -42,6 +45,7 @@ public class Tabs extends AppCompatActivity implements View.OnClickListener{
         specs.setContent(R.id.tab3);
         specs.setIndicator("Add a Tab");
         th.addTab(specs);
+        start = 0;
     }
 
     @Override
@@ -62,10 +66,14 @@ public class Tabs extends AppCompatActivity implements View.OnClickListener{
             break;
 
             case R.id.bStartWatch:
-
+                start = System.currentTimeMillis();
             break;
             case R.id.bStopWatch:
-
+                stop = System.currentTimeMillis();
+                if (start != 0){
+                    long result = stop - start;
+                    showResults.setText(Long.toString(result));
+                }
             break;
         }
     }
