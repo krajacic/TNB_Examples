8f6238e927b7affa887d485e2d74c04fc3886b97
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-01 11:14:47 +0100

    SlidingDrawer Example - tut81 TNB

---
+++ app/src/main/java/cawabanga/com/tnb_examples/Slider.java
@@ -0,0 +1,15 @@
+package cawabanga.com.tnb_examples;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by croatan on 1.3.2016. TNB_Examples.
+ */
+public class Slider extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sliding);
+    }
+}
