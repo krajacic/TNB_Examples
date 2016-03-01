96216b190ddf9534c279ec0e5e403019f9ed6a1b
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-01 13:31:36 +0100

    Setting up the TabHost in Java - tut85 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Tabs.java
@@ -2,6 +2,7 @@ package cawabanga.com.tnb_examples;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
 
 /**
  * Created by croatan on 1.3.2016. TNB_Examples.
@@ -11,5 +12,21 @@ public class Tabs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabs);
+        TabHost th = (TabHost) findViewById(R.id.tabHost);
+        th.setup();
+        TabHost.TabSpec specs = th.newTabSpec("tag1");
+        specs.setContent(R.id.tab1);
+        specs.setIndicator("StopWatch");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("tag2");
+        specs.setContent(R.id.tab2);
+        specs.setIndicator("Tab 2");
+        th.addTab(specs);
+
+        specs = th.newTabSpec("tag3");
+        specs.setContent(R.id.tab3);
+        specs.setIndicator("Add a Tab");
+        th.addTab(specs);
     }
 }
