7a2bcf955a4a0ba7caf2dd52227c8d6852edb273
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-04 14:30:46 +0100

    Spinners and ArrayAdapter - tut105 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/ExternalData.java
+++ app/src/main/java/cawabanga/com/tnb_examples/ExternalData.java
@@ -3,6 +3,8 @@ package cawabanga.com.tnb_examples;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 /**
@@ -12,6 +14,9 @@ public class ExternalData extends AppCompatActivity {
 
     private TextView canWrite, canRead;
     private String state;
+    boolean canW, canR;
+    Spinner spinner;
+    String[] paths = {"Music", "Pictures", "Downloads"}; //Options where we can save our datas
 
 
     @Override
@@ -25,13 +30,22 @@ public class ExternalData extends AppCompatActivity {
             //Media mounted means that we can read and write
             canRead.setText("true");
             canWrite.setText("true");
+            canW = canR = true;
         } else if (state.equals(Environment.MEDIA_MOUNTED_READ_ONLY)){
             //read but CAN NOT Write
             canRead.setText("true");
             canWrite.setText("false");
+            canW = false;
+            canR =  true;
         } else {
             canRead.setText("false");
             canWrite.setText("false");
+            canW = canR = false;
         }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(ExternalData.this, android.R.layout.simple_spinner_item, paths); //parameters (Context, type of spinner, string Array)
+
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
     }
 }
