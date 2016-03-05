27c2004b0ba7914e7e893715a178ab6479265d95
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-05 15:17:57 +0100

    SQLite class implementing SQLiteOpenHelper - tut113 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/HotOrNot.java
+++ app/src/main/java/cawabanga/com/tnb_examples/HotOrNot.java
@@ -1,5 +1,9 @@
 package cawabanga.com.tnb_examples;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by croatan on 5.3.2016. TNB_Examples.
  */
@@ -12,5 +16,31 @@ public class HotOrNot {
     private static final String DATABASE_NAME = "HotOrNotdb";
     private static final String DATABASE_TABLE = "peopleTable";
     private static final int DATABASE_VERSION = 1;
+
+
+    private DbHelper ourHelper;
+    private final Context ourContext;
+    private SQLiteDatabase ourDatabase;
+
+    private static class DbHelper extends SQLiteOpenHelper{
+
+        public DbHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        }
+    }
+
+    public HotOrNot(Context c){
+        ourContext = c;
+    }
 
 }
