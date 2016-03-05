f7ca2dc26ab19fea413c6c2da9d206715fd5fe71
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-05 15:08:48 +0100

    Setting up SQLite Database Variables - tut112 TNB

--- app/src/main/java/cawabanga/com/tnb_examples/HotOrNot.java
+++ app/src/main/java/cawabanga/com/tnb_examples/HotOrNot.java
@@ -4,4 +4,13 @@ package cawabanga.com.tnb_examples;
  * Created by croatan on 5.3.2016. TNB_Examples.
  */
 public class HotOrNot {
+
+    public static final String KEY_ROWID = "_id"; // public means other class can access it static we don't wanna change Final-that never gonna change
+    public static final String KEY_NAME = "persons_name";
+    public static final String KEY_HOTNESS = "persons_hotness";
+
+    private static final String DATABASE_NAME = "HotOrNotdb";
+    private static final String DATABASE_TABLE = "peopleTable";
+    private static final int DATABASE_VERSION = 1;
+
 }
