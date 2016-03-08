40bb02016a6bc096c955289af9e7c647db6ac2ca
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-08 21:31:40 +0100

    SAXParserFactory and XMLReader - tut157TNB   //This may not work :S

--- app/src/main/java/cawabanga/com/tnb_examples/WeatherXMLParsing.java
+++ app/src/main/java/cawabanga/com/tnb_examples/WeatherXMLParsing.java
@@ -7,6 +7,14 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+import java.net.URL;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
 /**
  * Created by croatan on 8.3.2016. TNB_Examples.
  */
@@ -24,6 +32,7 @@ public class WeatherXMLParsing extends AppCompatActivity implements View.OnClick
         tv = (TextView) findViewById(R.id.tvWeather);
         city = (EditText) findViewById(R.id.etCity);
         country = (EditText) findViewById(R.id.etCountry);
+        b.setOnClickListener(this);
     }
 
     @Override
@@ -34,6 +43,19 @@ public class WeatherXMLParsing extends AppCompatActivity implements View.OnClick
         StringBuilder URL = new StringBuilder(baseURL);
         URL.append(cty + "," + cnt + "&appid=44db6a862fba0b067b1930da0d769e98");
         String fullUrl = URL.toString();
+        try {
+            URL website = new URL(fullUrl);
+            //getting xmlreader to parse data
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser sp =  spf.newSAXParser();
+            XMLReader xr = sp.getXMLReader();
+            HandlingXMLStuff doingWork = new HandlingXMLStuff();
+            xr.setContentHandler(doingWork);
+            xr.parse(new InputSource(website.openStream()));
+
+        }catch (Exception e){
+            tv.setText("Error!");
+        }
 
 
     }
