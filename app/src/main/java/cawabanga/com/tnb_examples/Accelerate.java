da3c10d0183c11419a7f255d33a3e02aae44cb5a
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-08 18:17:35 +0100

    Setting up Sensor Manager - tut126TNB

--- app/src/main/java/cawabanga/com/tnb_examples/Accelerate.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Accelerate.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.SurfaceHolder;
@@ -62,6 +63,11 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyBringBackSurface(this));
+        SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0){ //if size is 0 (zero) than there is no sensor available on device
+            Sensor s = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            sm.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
+        }
     }
 
     @Override
