526abfa02ad3ede6255ec2d1c1381ce3985f8de4
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-08 20:14:27 +0100

    JSONArrays and JSONObjects - tut151TNB //This may not work :S

--- app/src/main/java/cawabanga/com/tnb_examples/HttpExample.java
+++ app/src/main/java/cawabanga/com/tnb_examples/HttpExample.java
@@ -3,16 +3,21 @@ package cawabanga.com.tnb_examples;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
 
+import cz.msebera.android.httpclient.HttpEntity;
+import cz.msebera.android.httpclient.HttpResponse;
 import cz.msebera.android.httpclient.client.ClientProtocolException;
 import cz.msebera.android.httpclient.client.HttpClient;
 import cz.msebera.android.httpclient.client.methods.HttpGet;
+import cz.msebera.android.httpclient.util.EntityUtils;
 
 /**
  * Created by croatan on 8.3.2016. TNB_Examples.
@@ -36,5 +41,17 @@ public class HttpExample extends AppCompatActivity {
         StringBuilder url = new StringBuilder(URL);
 
         HttpGet get = new HttpGet(url.toString());
+        HttpResponse r = client.execute(get);
+        int status = r.getStatusLine().getStatusCode();
+        if (status == 200 ){
+            HttpEntity e = r.getEntity();
+            String data = EntityUtils.toString(e);
+            JSONArray timeline = new JSONArray(data);
+            JSONObject last = timeline.getJSONObject(0);
+            return last;
+        }else{
+            Toast.makeText(HttpExample.this, "error", Toast.LENGTH_LONG);
+            return null;
+        }
     }
 }
