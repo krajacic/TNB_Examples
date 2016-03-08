60426fa02087c12c5b2c6d5fd29a81ae74789063
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-08 21:25:46 +0100

    Setting up a parsed XML data collection class - tut156TNB                       //This may not work :S

--- app/src/main/java/cawabanga/com/tnb_examples/XMLDataCollected.java
+++ app/src/main/java/cawabanga/com/tnb_examples/XMLDataCollected.java
@@ -4,4 +4,19 @@ package cawabanga.com.tnb_examples;
  * Created by croatan on 8.3.2016. TNB_Examples.
  */
 public class XMLDataCollected {
+    int temp = 0;
+    String city = null;
+
+    public void setCity(String c){
+        city = c;
+
+    }
+
+    public void setTemp(int t){
+        temp = t;
+    }
+
+    public String dataToString(){
+        return "In " + city + " current temperature is " + temp + " degrees.";
+    }
 }
