73b119f0f884371b075249d0ee6395d62101409e
Jurica Krajačić <krajacic_jurica@yahoo.com>
2016-03-08 18:34:14 +0100

    Finishing Accelerometer and unregistering - tut128TNB

--- app/src/main/java/cawabanga/com/tnb_examples/Accelerate.java
+++ app/src/main/java/cawabanga/com/tnb_examples/Accelerate.java
@@ -20,20 +20,22 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
 
     float x, y, sensorX, sensorY;
     Bitmap ball;
+    SensorManager sm;
+    MyBringBackSurface ourSurfaceView;
 
-    public class MyBringBackSurface extends SurfaceView implements Runnable{
+    public class MyBringBackSurface extends SurfaceView implements Runnable {
         SurfaceHolder ourHolder;
         Thread ourThread = null;
         boolean isRunning = false;
 
-        public MyBringBackSurface(Context context){
+        public MyBringBackSurface(Context context) {
             super(context);
             ourHolder = getHolder();
         }
 
-        public void pause(){
+        public void pause() {
             isRunning = false;
-            while (true){
+            while (true) {
                 try {
                     ourThread.join();
                 } catch (InterruptedException e) {
@@ -44,7 +46,8 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
 
             ourThread = null;
         }
-        public void resume(){
+
+        public void resume() {
             isRunning = true;
             ourThread = new Thread(this);
             ourThread.start();
@@ -52,13 +55,15 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
 
         @Override
         public void run() {
-            while (isRunning){
+            while (isRunning) {
                 if (!ourHolder.getSurface().isValid())
                     continue;
 
                 Canvas canvas = ourHolder.lockCanvas();
                 canvas.drawRGB(2, 2, 150);
-
+                float centerX = canvas.getWidth() / 2;
+                float centerY = canvas.getHeight() / 2;
+                canvas.drawBitmap(ball, centerX + sensorX*20, centerY + sensorY*20, null);
                 ourHolder.unlockCanvasAndPost(canvas);
             }
         }
@@ -68,14 +73,24 @@ public class Accelerate extends AppCompatActivity implements SensorEventListener
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(new MyBringBackSurface(this));
-        SensorManager sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        if (sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0){ //if size is 0 (zero) than there is no sensor available on device
+        sm = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        if (sm.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) { //if size is 0 (zero) than there is no sensor available on device
             Sensor s = sm.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
             sm.registerListener(this, s, SensorManager.SENSOR_DELAY_NORMAL);
         }
 
         ball = BitmapFactory.decodeResource(getResources(), R.drawable.ball); //setup bitmap
         x = y = sensorY = sensorX = 0;
+
+        ourSurfaceView = new MyBringBackSurface(this);
+        ourSurfaceView.resume();
+        setContentView(ourSurfaceView);
+    }
+
+    @Override
+    protected void onPause() {
+        sm.unregisterListener(this);
+        super.onPause();
     }
 
     @Override
